5d1a2929dad1174631a12c0f6261aae09c10afa2
nikita <nikitaorlov59@gmail.com>
2025-11-20 06:39:52 +0500

    Добавьте файлы проекта.

---
+++ l5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace l5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Workbook wb;
+            try
+            {
+                wb = new Workbook("LR5-var9.xls");
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine("Error loading workbook: " + ex.Message);
+                return;
+            }
+        }
+    }
+}
